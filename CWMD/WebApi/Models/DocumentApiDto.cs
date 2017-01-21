4ffc8e98d14a2485cab9a428da5df414f0bdc46b
Angel <Angel@DESKTOP-0M48KMD>
2017-01-21 17:44:24 +0200

    Added documents table in work-zone, also added one new field to documentsDto and added one JS service for documents.

--- CWMD/WebApi/Models/DocumentApiDto.cs
+++ CWMD/WebApi/Models/DocumentApiDto.cs
@@ -10,32 +10,16 @@ namespace WebApi.Models
 {
     public class DocumentApiDto
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
-        [Required]
         public string Name { get; set; }
-
-        [Required]
         public string Type { get; set; }
-
-        [DataType(DataType.Date)]
-        [Display(Name = "Added Date")]
         public DateTime AddedDate { get; set; }
-
-        [DataType(DataType.Date)]
-        [Display(Name = "Updated Date")]
         public DateTime UpdatedDate { get; set; }
-
-        [ForeignKey("Persons")]
         public int PersonId { get; set; }
         public virtual Person Person { get; set; }
-
-        [ForeignKey("Documents")]
         public int PrincipalDocumentId { get; set; }
         public virtual Document Document { get; set; }
-
         public string Version { get; set; }
+        public string Flow { get; set; }
     }
 }
